e99a3ff0a723137b12e7711e87c5b60dcb4ddbc7
Dexter Gramfors <dexter.gramfors@trioptima.com>
2021-01-29 22:01:56 -0500

    Add twin stick controls

--- Entities/Fan/Fan.gd
+++ Entities/Fan/Fan.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
 var speed = 1000
+onready var fan = $Fan
 
 func _physics_process(delta):
-	var direction := Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
-		direction += Vector2.LEFT
-	if Input.is_action_pressed("ui_right"):
-		direction += Vector2.RIGHT
-	var velocity = direction * 2500 * delta
-	move_and_collide(velocity)
+	var direction = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var velocity = direction * 500 * delta
+	move_and_collide(Vector2(velocity, 0))
+
+	var aim_direction = Input.get_action_strength("aim_right") - Input.get_action_strength("aim_left")
+	print(Input.get_joy_axis(0, 1))
+	fan.rotation = aim_direction
